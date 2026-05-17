d39ffb75c20c46cb50b8710f79fd3482724af6d3
Frankiga-GA <mguistv@gmail.com>
2026-05-17 11:19:23 -0500

    Fix Express wildcard route for Render

--- server/index.js
+++ server/index.js
@@ -248,7 +248,7 @@ No uses markdown dentro del JSON.
 // Serve frontend in production
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
